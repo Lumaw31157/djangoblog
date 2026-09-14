b00fb6ea979dced159f5fef3e8cb807d9132cb2a
Lumaw6100 <lumaw31157@gmail.com>
2026-09-14 11:59:39 +0900

    PostgreSQL Database, Post Model

--- config/settings.py
+++ config/settings.py
@@ -81,7 +81,19 @@ DATABASES = {
     }
 }
 
+# Database
+# https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
 
