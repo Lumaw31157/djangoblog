c0bb30e6ff8957be59fe6bacad365a617ef37fb1
Lumaw6100 <lumaw31157@gmail.com>
2026-09-16 14:04:51 +0900

    Uptade Project

--- blog/models.py
+++ blog/models.py
@@ -11,10 +11,10 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self , *args, **kwargs):
+    def save(self , *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
-            super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):        
         return self.title
